dcc291a96935482e1c7c3b103531a216e390b910
lepitaaar <leekus1005@gmail.com>
2025-09-14 17:02:45 +0900

    feat: 좌석 등급에 맞는 가격인지 확인하는 테스트코드 작성

---
+++ src/test/java/movie/model/SeatTest.java
@@ -0,0 +1,25 @@
+package movie.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeatTest {
+    @Test
+    void S등급_좌석_가격이_맞는지_확인한다() {
+        Seat seatS = new Seat(SeatGrade.S);
+        assertEquals(18000, seatS.getGrade().getPrice());
+    }
+
+    @Test
+    void A등급_좌석_가격이_맞는지_확인한다() {
+        Seat seatA = new Seat(SeatGrade.A);
+        assertEquals(15000, seatA.getGrade().getPrice());
+    }
+
+    @Test
+    void B등급_좌석_가격이_맞는지_확인한다() {
+        Seat seatB = new Seat(SeatGrade.B);
+        assertEquals(12000, seatB.getGrade().getPrice());
+    }
+}
