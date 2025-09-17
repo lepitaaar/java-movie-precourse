3606584ab2a656b331b4190f14c0c6ad53b16588
lepitaaar <leekus1005@gmail.com>
2025-09-17 21:54:17 +0900

    feat: 좌석 등급을 배치 규칙에 따라 변경
    SeatGradeRule을 상속 받은 규칙을 정의하여 확장성있게 추가하였습니다

--- src/main/java/movie/model/Cinema.java
+++ src/main/java/movie/model/Cinema.java
@@ -5,20 +5,28 @@ import java.util.HashMap;
 public class Cinema {
     private byte startTime;
     private byte endTime;
-    private char endRow;
-    private int column;
+
+    private char maxRow;
+    private int maxColumn;
+
+    private SeatGradeRule seatGradeRule;
 
     //                    row,    column
     private final HashMap<String, Seat[]> seats = new HashMap<>();
 
-    public Cinema(byte startTime, byte endTime, char endRow, int column) {
+    public Cinema(byte startTime, byte endTime, char maxRow, int maxColumn, SeatGradeRule seatGradeRule) {
         this.startTime = startTime;
         this.endTime = endTime;
-        this.endRow = endRow;
-        this.column = column;
+        this.maxRow = maxRow;
+        this.maxColumn = maxColumn;
+        this.seatGradeRule = seatGradeRule;
 
-        for (int i = 'A'; i <= endRow; i++) {
-            seats.put(String.valueOf((char) i), new Seat[this.column]);
+        for (int i = 'A'; i <= maxRow; i++) {
+            Seat[] _seat = new Seat[this.maxColumn];
+            for (int j = 0; j < this.maxColumn; j++) {
+                _seat[j] = new Seat(seatGradeRule.getSeatGrade(String.valueOf((char) i), String.valueOf(maxRow)));
+            }
+            seats.put(String.valueOf((char) i), _seat);
         }
     }
 }
