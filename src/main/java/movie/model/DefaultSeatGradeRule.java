a524aa878b2d08ed9596294542f8ce21f2c253ce
lepitaaar <leekus1005@gmail.com>
2025-09-18 01:17:56 +0900

    feat: 좌석 등급을 매길 기본 규칙 추가

---
+++ src/main/java/movie/model/DefaultSeatGradeRule.java
@@ -0,0 +1,18 @@
+package movie.model;
+
+public class DefaultSeatGradeRule implements SeatGradeRule {
+    @Override
+    public SeatGrade getSeatGrade(char row, char maxRow) {
+        int rowValue = row - 'A';
+        int maxRowValue = maxRow - 'A';
+        int sectionSize = (maxRowValue + 1) / 3;
+
+        if (rowValue < sectionSize) {
+            return SeatGrade.B;
+        } else if (rowValue < sectionSize * 2) {
+            return SeatGrade.S;
+        } else {
+            return SeatGrade.A;
+        }
+    }
+}
